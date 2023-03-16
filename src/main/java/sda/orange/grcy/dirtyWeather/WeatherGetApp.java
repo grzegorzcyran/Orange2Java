fd7b4c1d5dc5936504d79c987cdb55058c802607
Grzegorz Cyran <grzegorz.cyran@interia.pl>
2023-03-16 20:48:32 +0100

    Weather app run

--- src/main/java/sda/orange/grcy/dirtyWeather/WeatherGetApp.java
+++ src/main/java/sda/orange/grcy/dirtyWeather/WeatherGetApp.java
@@ -1,6 +1,8 @@
 package sda.orange.grcy.dirtyWeather;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -28,7 +30,29 @@ public class WeatherGetApp {
              * INFO - pobranie informacji - np na temat dostępnych tzw. endpointów czyli adresów pod którymi
              *        można wysłać GET, POST, ...
              */
+            int responseCode = conn.getResponseCode();
 
+            /**
+             * Kody odpowiedzi HTTP:
+             * 100 - info
+             * 200 - OK
+             * 300 - przekierowanie
+             * 400 - błąd użytkownika
+             * 500 - błąd serwera
+             */
+
+            System.out.println("Kod odpowiedzi:" + responseCode);
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = reader.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                reader.close();
+                System.out.println(response);
+            }
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
